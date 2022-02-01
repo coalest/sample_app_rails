0c9d533559082b3dd62d3b9dabb5b48b7cca421a
coalest <c.streiff@pm.me>
2022-02-01 12:29:50 +0100

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign Up")
   end
 end
