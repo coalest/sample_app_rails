c64412eafb9aa762c0b107dac60e6b6a4709d220
coalest <c.streiff@pm.me>
2022-02-02 16:39:47 +0100

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid params dont get saved to db" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  '',
+                                         email: 'user@invalid',
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid inputs do get saved to db" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  'Testy McGee',
+                                         email: 'user@valid.com',
+                                         password:              "foobarbaz",
+                                         password_confirmation: "foobarbaz" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
+  end
+end
